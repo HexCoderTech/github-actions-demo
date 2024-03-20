551f37706fe6d8206a7ddd069b3dfff5f3578a43
Pasindu Madhuwantha <73535785+PasinduMaduwantha@users.noreply.github.com>
2024-03-21 00:28:54 +0530

    -

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -6,7 +6,7 @@ def test_format_weather():
     city = "Los Angeles"
     data = {"weather": [{"description": "sunny"}], "main": {"temp": 25}}
     expected = (
-                "The weather  in Los Angeles is sunny "
+                "The weather in Los Angeles is sunny "
                 "with a temperature of 25 Celcius."
                 )
     assert format_weather(city, data) == expected
